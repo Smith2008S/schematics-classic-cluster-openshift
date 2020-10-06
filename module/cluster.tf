2eb845777fd4e14ec8b4177cef8d750824ddcd4f
Smith2008S <60987042+Smith2008S@users.noreply.github.com>
2020-10-06 15:27:09 -0500

    Update cluster.tf

--- module/cluster.tf
+++ module/cluster.tf
@@ -10,8 +10,8 @@ resource "ibm_container_cluster" "cluster" {
   machine_type      = "${var.machine_type}"
   hardware          = "${var.hardware}"
   kube_version      = "${var.kube_version}"
-  public_vlan_id    = "${var.public_vlan_id}"
-  private_vlan_id   = "${var.private_vlan_id}"
+#  public_vlan_id    = "${var.public_vlan_id}"
+#  private_vlan_id   = "${var.private_vlan_id}"
 }
 
 
