6aa50365d12f3129d964dc8a8fdcf77a7737f3bc
Smith2008S <60987042+Smith2008S@users.noreply.github.com>
2020-10-07 08:42:31 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,10 +3,13 @@
 # > This block needs to be here to use in schematics
 ##############################################################################
 
-provider "ibm" {
-  ibmcloud_api_key   = "${var.ibmcloud_api_key}"
+variable "ibmcloud_api_key" {
+  description = "Enter your IBM Cloud API Key, you can get your IBM Cloud API key using: https://cloud.ibm.com/iam#/apikeys"
 }
 
+provider "ibm" {
+  ibmcloud_api_key = var.ibmcloud_api_key
+}
 ##############################################################################
 
 
