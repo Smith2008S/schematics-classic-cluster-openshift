79c61e26cf8a6c30e7aeaf31857dd6311ee121d7
Smith2008S <60987042+Smith2008S@users.noreply.github.com>
2020-10-07 09:22:53 -0500

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -1,3 +1,11 @@
+provider "ibm" {
+  generation = 2
+}
+
+resource "ibm_is_vpc" "vpc1" {
+  name = "myvpc"
+}
+
 
 ##############################################################################
 # Create Cluster
@@ -6,6 +14,7 @@
 resource "ibm_container_cluster" "cluster" {
   name              = "${var.cluster_name}${random_id.name.hex}"
   datacenter        = var.datacenter
+  vpc_id            = ibm_is_vpc.vpc1.id
   default_pool_size = var.default_pool_size
   machine_type      = var.machine_type
   hardware          = var.hardware
